1e06a408e13b23b5c451c03ef500a82d41b8b56f
isaias_pruebas <isgb_codigo@hotmail.com>
2022-05-02 17:14:48 -0500

    Nombre del proyecto:my-app Descripcion:Practica de repaso sftkk

---
+++ src/main/java/com/isgb/myapp/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.isgb.myapp.entity;
+
+/**
+ * @author isgb
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilación del archivo testentity 
+	 */
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
